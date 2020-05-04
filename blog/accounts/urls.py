568d08a8a888260e85c8e09c3d93938063bd071e
CDinuwan <chanukadinuwan35@gmail.com>
2020-05-04 08:51:44 +0530

    Add user authentication form

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -4,5 +4,6 @@ from . import view
 app_name='accounts'
 
 urlpatterns=[
-    url(r'^signup/$',view.signup_view,name="signup")
+    url(r'^signup/$',view.signup_view,name="signup"),
+    url(r'^login/$',view.login_view,name="login"),
 ]
--- blog/accounts/view.py
+++ blog/accounts/view.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log the user in
+            return redirect('articles:list')
+    else:
+        form=UserCreationForm()
     form=UserCreationForm()
-    return render(request,'accounts/signup.html',{'from':form})
+    return render(request,'accounts/signup.html',{'form':form})
+
+def login_view(request):
+    if request.method=='POST':
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log in the user
+            return redirect('articles:list')
+    else:
+        form=AuthenticationForm()
+    return render(request,'accounts/login.html',{'form':form})
