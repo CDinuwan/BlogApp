b86927feb5178c9f2776d3051e78aec28885a259
CDinuwan <chanukadinuwan35@gmail.com>
2020-05-11 21:31:31 +0530

    Update the styles in blog app

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,13 +5,14 @@ from django.conf.urls import url,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import view as article_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^accounts/',include('accounts.urls')),
     url(r'^articles/',include('articles.urls')),
     url(r'^about/$',view.about),
-    url(r'^$',view.homepage),
+    url(r'^$',article_view.article_list,name="home"),
 ]
 
 urlpatterns+=staticfiles_urlpatterns()
